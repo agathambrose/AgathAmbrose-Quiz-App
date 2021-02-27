ad175c46573d86aa549c6ec44f388d819f488f42
Agatha Ambrose <agathambrose@gmail.com>
2021-02-27 02:04:50 +0100

    spinner touch-up

--- src/App.js
+++ src/App.js
@@ -27,13 +27,13 @@ const App = () => {
       <Router history = {history}>
         <Switch>
             <Route exact path ='/'>
-              <Suspense fallback={<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+              <Suspense fallback={<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
                   <Home/>
               </Suspense>
             </Route>
 
             <Route exact path = '/login'>
-              <Suspense fallback={<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+              <Suspense fallback={<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
               {authSuccessful && role === "User" ? 
               <Redirect to = '/quiz-landing-page'/> : 
                 authSuccessful && role === "Admin" ?
@@ -44,31 +44,31 @@ const App = () => {
             </Route>
 
             <Route exact path = '/register'>
-              <Suspense fallback={<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+              <Suspense fallback={<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
               {authSuccessful ? <Redirect to = '/'/> : <Register/>}
               </Suspense>
             </Route>
 
             <Route exact path = '/quiz-landing-page'>
-              <Suspense fallback={<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+              <Suspense fallback={<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
                 <QuizLandnPg/>
               </Suspense>
             </Route>
 
             <Route exact path = '/quiz'>
-              <Suspense fallback={<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+              <Suspense fallback={<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
                 <Quiz/>
               </Suspense>
             </Route>
 
             <Route exact path = '/admin'>
-              <Suspense fallback = {<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+              <Suspense fallback = {<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
                 <Admin/>
               </Suspense>
             </Route>
 
             <Route exact path = '/admin-landing-page'>
-            <Suspense fallback={<Spinner animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
+            <Suspense fallback={<Spinner style = {{marginTop: '5rem'}} animation="grow" variant="secondary" className='d-flex justify-content-center mx-auto' />}>
               <AdminLandnPage/>
             </Suspense>
           </Route>
